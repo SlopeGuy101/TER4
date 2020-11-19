9c22b11abe14f093b5e5c5181a4b6b447269c98c
SlopeGuy101 <60612052+SlopeGuy101@users.noreply.github.com>
2020-11-19 17:33:42 -0500

    Added handling for voice-interface calculator to STT.py

--- STT.py
+++ STT.py
@@ -7,17 +7,31 @@ sentence = sys.argv[1]
 op = sys.argv[2]
 
 if op == 1:
-    tanslate(sentence)
-if op == 2:
     speak(sentence)
+if op == 2:
+    transcribe(sentence)
+if op == 3:
+    statement = transcribe()
+    if "multiply" or "times" or "multiplied" in statement:
+        exit(statement + "x")
+    if "divide" or "divided" or "over" in statement:
+        exit(statement + "/")
+    if "to the" or "power" in statement:
+        exit(statement + "^")
+    if "add" or "plus" in statement:
+        exit(statement + "+")
+    if "subtract" or "minus" in statement:
+        exit(statement + "-")
+    else:
+        exit(1)
 
-def transcribe(input):
+def transcribe():
     r = sr.Recognizer
     mic = sr.Microphone
     with mic as source:
-        audio = r.listen(source)
+        input = r.listen(source)
         t_f_s = r.recognize_google(audio, language = 'en-us')
-    return audio
+    return input
 
 
 def speak(txt):
