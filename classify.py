18c75f31f25e522b0173d5cd4d0cd8422a50720e
SlopeGuy101 <60612052+SlopeGuy101@users.noreply.github.com>
2020-11-24 17:17:21 -0500

    Renamed Math supporting files to get rid of spaces

--- classify.py
+++ classify.py
@@ -17,7 +17,7 @@ class entry:
     #For math entries, check mathematical operation
     def get_op(self):
         words = self.cont.split(" ")
-        with open("Math Dictionary.txt") as dict:
+        with open("Math_Dictionary.txt") as dict:
             for i in words:
                 if words[i] in dict.split("\n"):
                     is_math = True
@@ -29,7 +29,7 @@ command = entry(transcribe().lower())
 #If the command is to perform a math operation, log the operation and
 #pass corresponding command code to BASH
 if command.is_math:
-    with open("Math Transfer.txt", "w") as trans:
+    with open("Math_Transfer.txt", "w") as trans:
         trans.write(command.cont)
         sys.stdout.write(command.get_op())
         sys.exit(0)
