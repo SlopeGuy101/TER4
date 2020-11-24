0569e6889bc76ac96331e8bf88ded76912af6ff0
SlopeGuy101 <60612052+SlopeGuy101@users.noreply.github.com>
2020-11-24 17:59:34 -0500

    Update BASH_Framework.sh

--- BASH_Framework.sh
+++ BASH_Framework.sh
@@ -1,4 +1,12 @@
 # ENSURE TO LAUNCH ALL SCRIPTS FROM SAME DIRECTORY
+
+#Checks if an error code was returned from math.java
+checkMath(){
+	if [[ $? -eq 1 ]]; then
+			python3 handle.py "Please provide two numbers and one operation in your equation" 1
+			main()
+}
+
 main(){
 	#Script only moves forward once motion is sensed
 	python3 sense_motion.py
@@ -7,23 +15,23 @@ main(){
 	
 	#If classify.py returned a math statement, evaluate accordingly
 	case $cmnd in
-		\*)
+		"*")
 			java math.java "1"
 			checkMath()
 			;;
-		\/)
+		"/")
 			java math.java "2"
 			checkMath()
 			;;
-		\+)
+		"+")
 			java math.java "3"
 			checkMath()
 			;;
-		\-)
+		"-")
 			java math.java "4"
 			checkMath()
 			;;
-		\^)
+		"^")
 			java math.java "5"
 			checkMath()
 			;;
@@ -31,11 +39,4 @@ main(){
 	
 	#main function recurses to acccomodate for future commands
 	main()
-	}
-	
-	#Checks if an error code was returned from math.java
-	checkMath(){
-		if [[ $? -eq 1 ]]; then
-				python3 handle.py "Please provide two numbers and one operation in your equation" 1
-				main()
-	}
+}
