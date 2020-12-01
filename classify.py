695d881a7e179478607fc1bb6f6b4fab49b1306a
SlopeGuy101 <60612052+SlopeGuy101@users.noreply.github.com>
2020-12-01 11:08:31 -0500

    debugging

--- classify.py
+++ classify.py
@@ -5,42 +5,55 @@ __version__ = "0.5 (alpha)"
 
 import sys
 import speech_recognition as sr
+from math import solve
+from sense_motion import sense
 
-#Method transcribes spoken text into string input
-def transcribe():
-    r = sr.Recognizer()
-    mic = sr.Microphone(device_index = 0)
 
-    #Listens from systems microphone, set to usb sound card for Raspberry Pi 4
-    with mic as source:
-        audio = r.listen(source)
-        t_f_s = r.recognize_google(audio, language = 'en-US')
-    return t_f_s
+def main():
+    #Only move into function when 
+    #sense()
+    #Method transcribes spoken text into string input
+    def transcribe():
+        r = sr.Recognizer()
+        mic = sr.Microphone()
 
-#Any possible entry will be an object of entry class
-class entry:
-    def __init__(self, content):
-        self.cont = content
+        #Listens from systems microphone, set to usb sound card for Raspberry Pi 4
+        with mic as source:
+            audio = r.listen(source)
+            t_f_s = r.recognize_google(audio, language = 'en-US')
+            print(t_f_s)
+        return t_f_s
 
-    #Class-level variable: all entries either are math or are not
-    is_math = False
+    #Any possible entry will be an object of entry class
+    class entry:
+        def __init__(self, content):
+            self.cont = content
+            self.op = self.get_op()
 
-    #For math entries, check mathematical operation
-    def get_op(self):
-        words = self.cont.split(" ")
-        with open("Math_Dictionary.txt") as dict:
-            for i in words:
-                if words[i] in dict.split("\n"):
-                    is_math = True
-                    return words[i]
+        #Class-level variable: all entries either are math or are not
+        is_math = False
 
-#A command is any instruction for the system to execute as a feature
-command = entry(transcribe().lower())
+        #For math entries, check mathematical operation
+        def get_op(self):
+            words = self.cont.split(" ")
+            with open("Math_Dictionary.txt") as dict:
+                dictionary = dict.read().split("\n")
+                #print(words)
+                for i in range(len(words)):
+                    for j in range(len(dictionary)):
+                        if words[i] == dictionary[j]:
+                            print(words[i])
+                            return words[i]
 
-#If the command is to perform a math operation, log the operation and
-#pass corresponding command code to BASH
-if command.is_math:
-    with open("Math_Transfer.txt", "w") as trans:
-        trans.write(command.cont)
-        sys.stdout.write(command.get_op())
-        sys.exit(0)
+    #A command is any instruction for the system to execute as a feature
+    command = entry(transcribe().lower())
+
+    #If the command is to perform a math operation, log the operation and
+    #pass corresponding command code to BASH
+    if command.is_math:
+        with open("Math_Transfer.txt", "w") as trans:
+            sentence = trans.read()
+    
+    main()
+
+main()
