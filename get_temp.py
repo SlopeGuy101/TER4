3a2a92bf7f5117d6d8dc7828baeab66e75713a6e
SlopeGuy101 <60612052+SlopeGuy101@users.noreply.github.com>
2021-01-22 12:49:53 -0500

    debugging

--- get_temp.py
+++ get_temp.py
@@ -4,5 +4,6 @@ arduino = serial.Serial('/dev/ttyACM0', 9600, timeout = .1)
 while True:
     data = arduino.readline()[:-2]
     if data:
-        temp = str(data[1:])
+        temp = str(data)[1:]
         speak(("It is" + temp + "in here"))
+        exit(0)
