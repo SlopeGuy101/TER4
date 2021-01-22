2ffb548f10476c7d0af9b69d3ccd69bcb0800739
SlopeGuy101 <60612052+SlopeGuy101@users.noreply.github.com>
2021-01-22 11:16:20 -0500

    update

--- get_temp.py
+++ get_temp.py
@@ -1,6 +1,7 @@
 import serial
 from handle import speak
 arduino = serial.Serial('/dev/ttyACM0', 115200, timeout = .1)
-data = arduino.readline()
 while True:
-    print(data)
+    data = arduino.readline()[:-2]
+    if data:
+        print(data)
