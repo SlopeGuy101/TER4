5d74e9d4d052f2bbc0cf69b8414b295f90fbb081
SlopeGuy101 <60612052+SlopeGuy101@users.noreply.github.com>
2021-01-22 12:51:05 -0500

    update

--- get_temp.py
+++ get_temp.py
@@ -5,5 +5,5 @@ while True:
     data = arduino.readline()[:-2]
     if data:
         temp = str(data)[1:]
-        speak(("It is" + temp + "in here"))
+        speak(("It is" + temp + "degrees in here"))
         exit(0)
